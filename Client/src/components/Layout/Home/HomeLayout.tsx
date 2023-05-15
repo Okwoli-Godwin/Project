73c6eba178628399fd7b8b6fd541d285f9e9b046
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-15 17:51:18 +0100

    pushed

--- Client/src/components/Layout/Home/HomeLayout.tsx
+++ Client/src/components/Layout/Home/HomeLayout.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Header, Footer } from '../../Static'
+import {Outlet} from "react-router-dom"
 
 const HomeLayout = () => {
   return (
-    <div>HomeLayout</div>
+      <div>
+          <Header />
+          <Outlet />
+          <Footer />
+    </div>
   )
 }
 
