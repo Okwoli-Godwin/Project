f1a18fe7242fb29f6b767c683c77dcd8e11c4b5d
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-15 18:32:58 +0100

    update

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import {Elements} from "./routes/Routes.tsx"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Elements}/>
+    {/* <App /> */}
   </React.StrictMode>,
 )
