9422d9077af55d9d34d814ab789ac67e7231ebce
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-16 10:33:45 +0100

    update

--- Client/src/pages/Landingpage/Homescreen.tsx
+++ Client/src/pages/Landingpage/Homescreen.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from './Hero'
 
 const Homescreen = () => {
   return (
-    <div>Homescreen</div>
+      <div style={{
+          overflow: "hidden"
+      }}>
+          <Hero />
+    </div>
   )
 }
 
